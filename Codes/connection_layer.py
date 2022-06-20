5afe7101f34526625a84af42848c467b9e477953
Yuankun Zou <Kunko2005@hotmail.com>
2022-06-20 15:13:18 +0800

    Bug fix

--- Codes/connection_layer.py
+++ Codes/connection_layer.py
@@ -1,4 +1,5 @@
 from tools import REDTEXT, WHITETEXT
+import math
 
 
 class AbstractProtocol:
@@ -12,21 +13,18 @@ class AbstractProtocol:
         else:
             return self.region1
 
-    def accessible_from(self, origin: 'AbstractRegion', current_distance: int = 0,
-                        source_region_record: dict['AbstractRegion', int] = None):
-        if source_region_record is None:
-            source_region_record = {}
-
+    def accessible_from(self, origin: 'AbstractRegion', current_distance: int = 0, path: list['AbstractRegion'] = None):
         res = {}
         source_region = self.other_side(origin)
-        # print(f'Getting view of {source_region} through {self}')
-        if source_region in source_region_record.keys() and current_distance >= source_region_record[source_region]:
-            # print('Meet the track')
+        print(f'Getting view of {source_region} through {self}')
+        if source_region in path:
+            print('Meet the path')
             return {}
-        else:
-            source_region_record[source_region] = current_distance
-        for region, distance in source_region.accessible_from(self, current_distance, source_region_record).items():
-            res[region] = distance + 1
+        path_copy = path[:]
+        path.append(source_region)
+        for region, distance in source_region.accessible_from(self, current_distance, path).items():
+            if region not in path_copy:
+                res[region] = distance + 1
         return res
 
     def place_between(self, region1, region2):
@@ -43,39 +41,59 @@ class AbstractRegion:
     def __init__(self, name: str, *args, add_self=True, **kwargs):
         self.na = name
         self.add_self = add_self
-        self.finished = False
         self.protocols: list[AbstractProtocol] = []
-        self.connect_dict: dict[AbstractRegion, tuple[int, list[AbstractProtocol]]] = {}
+        self.connect_dict: dict[AbstractProtocol, dict[AbstractRegion: int]] = {}
+        self.finished = False  # whether the connection dict has been generated
 
-    def accessible_from(self, origin: AbstractProtocol = None, current_distance: int = 0,
-                        source_region_record: dict['AbstractRegion', int] = None) -> dict['AbstractRegion', int]:
-        # print(f'Getting accessible regions from {self} through protocols except {origin}')
-        if source_region_record is None:
-            source_region_record = {self: 0}
+    def _generate_connection_dict(self,
+                                  current_distance: int = 0,
+                                  root_path: list['AbstractRegion'] = None):
+        assert not self.finished
+        top_level = False
+        if root_path is None:
+            root_path = []
+            top_level = True
+        for protocol in self.protocols:
+            path = root_path + [self]
+            print('****start a branch')
+            print(f'{self}, Processing Protocol:{protocol}')
+            current_distance += 1  # the distance to the next region
+            self.connect_dict[protocol] = protocol.accessible_from(self, current_distance, path)
+        if top_level:
+            self.finished = True
+
+    def accessible_from(self, origin, current_distance: int = 0, path: list['AbstractRegion'] = None) -> \
+            dict['AbstractRegion', int]:
+        print(f'Getting accessible regions from {self} through protocols except {origin}')
+        if not self.finished:
+            self._generate_connection_dict(current_distance, path)
+        else:
+            print(f'{self} already has connection dict')
 
         res = {}
         if self.add_self:
             res = {self: 0}
         for protocol in self.protocols:
             if protocol != origin:
-                # print(f'{self}, Processing Protocol:{protocol}')
-                current_distance += 1  # the distance to the next region
-                for region, distance in protocol.accessible_from(self, current_distance, source_region_record).items():
+                for region, distance in self.connect_dict[protocol].items():
                     if region != self and (region not in res.keys() or distance < res[region]):
                         res[region] = distance
-                        if origin is None:  # which means we are at the top level
-                            self.connect_dict[protocol][region] = distance
         return res
 
     def finish_construction(self):
         print(f'Start to process {self}')
-        self.accessible_from()
-        self.finished = True
+        self._generate_connection_dict()
         print(f'########{self} finished')
 
-    def find_protocol(self, target: 'AbstractRegion'):
+    def find_protocols(self, target: 'AbstractRegion') -> list[AbstractProtocol]:
         assert target != self
-        return self.guidance_dict[target]
+        candidates: list[AbstractProtocol] = []
+        min_distance = math.inf
+        for protocol in self.protocols:
+            if self.connect_dict[protocol][target] <= min_distance:
+                candidates.append(protocol)
+                min_distance = self.connect_dict[protocol][target]
+        return candidates
 
     def __repr__(self):
         return REDTEXT + self.na + WHITETEXT
@@ -83,7 +101,7 @@ class AbstractRegion:
     def detail_info(self):
         res = f'{self.na}. ' \
               f'Having protocols to: {", ".join([repr(protocol.other_side(self)) for protocol in self.protocols])}, '
-        for region, distance in self.accessible_from().items():
+        for region, distance in self.connect_dict.items():
             res += f'Having access to {region} in distance {distance} '
         return res
 
@@ -110,12 +128,20 @@ if __name__ == '__main__':
         host.add_region('B')
         host.add_region('C')
         host.add_region('D')
+        host.add_region('E')
+        host.add_region('F')
         host.connect('A', 'B')
         host.connect('B', 'C')
         host.connect('C', 'D')
-        host.connect('D', 'A')
+        host.connect('D', 'E')
+        host.connect('E', 'F')
+        host.connect('F', 'A')
+        host.connect('B', 'E')
 
         for region in host.regions.values():
             region.finish_construction()
         for region in host.regions.values():
             print(region.connect_dict)
+            print(region.finished)
+
+        print(host.regions['A'].find_protocols(host.regions['E']))
